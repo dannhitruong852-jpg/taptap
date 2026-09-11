5f1681e32844f7167a861d4d53f80213b571a6bf
dannhitruong852-jpg <dannhitruong852@gmail.com>
2026-09-12 00:52:33 +0800

    test: define voice batch partition behavior

---
+++ voice-pipeline/tests/test_batching.py
@@ -0,0 +1,24 @@
+import unittest
+
+from batching import partition_segments
+
+
+class BatchingTests(unittest.TestCase):
+    def test_partition_segments_splits_24_into_four_stable_shards(self):
+        segments = [f's{i:02d}' for i in range(1, 25)]
+        shards = partition_segments(segments, 4)
+        self.assertEqual(len(shards), 4)
+        self.assertEqual([len(s) for s in shards], [6, 6, 6, 6])
+        self.assertEqual(shards[0], segments[:6])
+        self.assertEqual(shards[3], segments[18:])
+
+    def test_partition_segments_preserves_order_without_duplicates(self):
+        segments = [f'x{i}' for i in range(7)]
+        shards = partition_segments(segments, 3)
+        flattened = [item for shard in shards for item in shard]
+        self.assertEqual(flattened, segments)
+        self.assertEqual(len(flattened), len(set(flattened)))
+
+
+if __name__ == '__main__':
+    unittest.main()
