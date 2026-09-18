6743f640cc38e8d7058800ec0b866a4ef80995dc
dannhitruong852-jpg <dannhitruong852@gmail.com>
2026-09-18 18:06:44 +0800

    fix: version inline phrase resource cache

--- kaoyan-reader-v1/static-resource-cache.js
+++ kaoyan-reader-v1/static-resource-cache.js
@@ -1,4 +1,4 @@
-const DEFAULT_NAMESPACE='kaoyan-static-active-sentence-20260918-v2';
+const DEFAULT_NAMESPACE='kaoyan-static-inline-phrase-20260918-v1';
 
 export function createStaticResourceCache({
   cacheStorage=typeof caches!=='undefined'?caches:null,
