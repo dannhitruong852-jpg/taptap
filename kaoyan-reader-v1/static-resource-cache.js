c6701b755b4b85a7d5e1080c8e32a70fbc263fe3
dannhitruong852-jpg <dannhitruong852@gmail.com>
2026-09-18 23:51:14 +0800

    release(reader): publish continuous saved phrase marks

--- kaoyan-reader-v1/static-resource-cache.js
+++ kaoyan-reader-v1/static-resource-cache.js
@@ -1,4 +1,4 @@
-const DEFAULT_NAMESPACE='kaoyan-static-phrase-study-continuous-20260918-v1';
+const DEFAULT_NAMESPACE='kaoyan-static-phrase-study-continuous-20260918-v2';
 
 export function createStaticResourceCache({
   cacheStorage=typeof caches!=='undefined'?caches:null,
