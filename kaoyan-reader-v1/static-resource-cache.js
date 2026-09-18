8aba9313f78e39f47895f20294d404b639ce4fd4
dannhitruong852-jpg <dannhitruong852@gmail.com>
2026-09-18 18:08:19 +0800

    feat: publish inline phrase marks

--- kaoyan-reader-v1/static-resource-cache.js
+++ kaoyan-reader-v1/static-resource-cache.js
@@ -1,4 +1,4 @@
-const DEFAULT_NAMESPACE='kaoyan-static-phrase-book-20260918-v1';
+const DEFAULT_NAMESPACE='kaoyan-static-inline-phrase-20260918-v1';
 
 export function createStaticResourceCache({
   cacheStorage=typeof caches!=='undefined'?caches:null,
